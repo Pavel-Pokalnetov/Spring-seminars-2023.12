d6aacd9b249e7e79c528d91e95d182da9a4220cc
Pavel-Pokalnetov <pokalnetovps@gmail.com>
2024-01-22 23:14:31 +0500

    ++++

--- Урок 4. Spring MVC. Использование шаблонизатора Thymeleaf/demo/target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS products (
-    title varchar(256) NOT NULL,
-    article varchar(50) NOT NULL PRIMARY KEY,
-    description text NOT NULL,
-    cost float,
-    imageURL varchar(256)
-);
-
