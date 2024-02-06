db05b0a4d4d1f7022da76f4a5d5c4975f3b7cf4f
Pavel-Pokalnetov <redx@mail.ru>
2024-02-06 22:16:05 +0500

    fin

--- HW8/PersonalNotes/src/main/java/ru/gb/pokalnetovps/PersonalNotes/aspects/TrackUserAction.java
+++ HW8/PersonalNotes/src/main/java/ru/gb/pokalnetovps/PersonalNotes/aspects/TrackUserAction.java
@@ -6,7 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- *  Объявление аннотации
+ Аннотация TrackUserAction используется для отслеживания действий пользователя в программе.
+ Она может быть применена к методам, которые выполняют определенные действия пользователя,
+ и затем эти данные могут быть использованы для создания отчетов или других целей.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
